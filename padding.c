470c39981f1682574e8dd5b65b5f8649e632111e
kbarry <g00339811@gmit.ie>
2019-03-11 20:12:33 +0000

    Reading file 64 bits at a time

--- padding.c
+++ padding.c
@@ -8,27 +8,42 @@
 /**
  * x to access as 2 8 bit integers.
  * y to access as a 16 bit integer.
+ * 
+ * This union allows every element to be saved in same memory location.
  */
-union point {
-    uint8_t x[64];  	// 8*64 = 512 bits
-    uint32_t y[16];     // 8*16 = 128 bits
-    uint64_t s[8]       // 64*8 = 512 bits
+union msgblock {
+    uint8_t     e[64];  // 8*64 = 512 bits
+    uint32_t    t[16]; // 8*16 = 128 bits
+    uint64_t    s[8];   // 64*8 = 512 bits
 };
 
-int main(int argc, char *argv[]){
-    FILE* file ;
+int main(int argc, char *argv[])
+{
+    // Define union message block instance.
+    union msgblock M;
+
+    // Used in future 
+    uint64_t nobytes;
+
+    // File pointer
+    FILE* file;
     char c;
 
     // Open the file.
-    file = fopen(argv[1],"r");
-    // TODO add check file success
+    file = fopen(argv[1], "r");
+    // TODO add check file success!!!!!
 
-    printf("%c\n",fread(&c,1,1,file));
+    // TODO must adaptdeal with error f.error!!!!!!
+    while (!feof(file))
+    {
+        nobytes = fread(M.e, 1, 64, file);
 
+        // Number of bytes that have been read.
+        printf("%11u\n", nobytes);
+    }
 
     // Close the file.
     fclose(file);
-
 
     return 0;
 }
