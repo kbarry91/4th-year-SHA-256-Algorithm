0b48450091ec05d99ea6d7735a2617d89d9916fe
kbarry <g00339811@gmit.ie>
2019-03-22 02:23:53 +0000

    Completed padding

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # 4th-year-SHA-256-Algorithm
 ## References
 - (ch maj)[https://crypto.stackexchange.com/questions/5358/what-does-maj-and-ch-mean-in-sha-256-algorithm/5360]
+  
+## Preprocessing
+- padding the meesage
+- parsing msg into msg blocks
+- setting initial hash value
+
+### Padding the message(To become a 51 bit padded message)
+- apepend "1" bit to end
+- add enough "0"s so left with 64 bits at the end
+- In remaining 64 encode length of msg in binary big indian
--- padding.c
+++ padding.c
@@ -6,15 +6,27 @@
 #include <stdint.h>
 
 /**
- * x to access as 2 8 bit integers.
- * y to access as a 16 bit integer.
+ * e to access as 8 bit integers.
+ * t to access as 32 bit integer.
+ * s to access as 64 bit integer.
  * 
  * This union allows every element to be saved in same memory location.
+ * Values are the same but being read differently.
+ * 
  */
 union msgblock {
     uint8_t e[64];  // 8*64 = 512 bits
-    uint32_t t[16]; // 8*16 = 128 bits
+    uint32_t t[16]; // 32*16 = 512 bits
     uint64_t s[8];  // 64*8 = 512 bits
+};
+
+// Flags to track state of padding status.stores(0,1,2,3)
+enum status
+{
+    READ,
+    PAD0,
+    PAD1,
+    FINISH
 };
 
 int main(int argc, char *argv[])
@@ -22,29 +34,32 @@ int main(int argc, char *argv[])
     // Define union message block instance.
     union msgblock M;
 
-    // Current number of bytes read.
+    // Current number of bytes read from fread.
     uint64_t nobytes;
 
     // Current number of bits read.
     uint64_t nobits = 0;
 
-    // File pointer
-    FILE *file;
-    char c;
+    enum status S = READ; // set status to read
 
+    // File pointer
+    FILE* file;
+    char c;
+    int i;
     // Open the file.
     file = fopen(argv[1], "r");
     // TODO add check file success!!!!!
 
     // TODO must adaptdeal with error f.error!!!!!!
     // As of section 5.2.1
-    while (!feof(file))
+    while (S == READ)
     {
         nobytes = fread(M.e, 1, 64, file);
+        printf("Read %2llu bytes\n", nobytes);
         // Add 8 bits for each byte;
         nobits = nobits + (nobytes * 8);
+
         
-        printf("Read %2llu bytes\n", nobytes);
         // Check if less than 56 bytes.
         if (nobytes < 56)
         {
@@ -56,28 +71,61 @@ int main(int argc, char *argv[])
 
             while (nobytes < 56)
             {
-                nobytes = nobytes + 1; // Add one as index into block.
+                nobytes = nobytes + 1; // Add one as index into block.((actually loop var))
                 M.e[nobytes] = 0x00;   // Set all bytes to 0.
             }
             // Set last 4 bytes as 64 biit integer as number of bits read from file.
 
-            // Set last element to nobits.
-            M.s[7] = nobits;
+            // Set last element to nobits(number of bits in origonal msg).
+            M.s[7] = nobits; // TODO  make sure its big indian integer
+            // set to finish stat
+            S = FINISH;
         }
-        // Number of bytes that have been read from file.
-        //printf("%11u\n", nobytes);
-    }
+        // If 56 to 64 bytes read we have to have an extra message block full of padding
+        // cannot append "1" and add a 64 bit integer to the original message block
+        else if (nobytes < 64)
+        {
+            S = PAD0;            // need another message block
+            M.e[nobytes] = 0x80; // append 1
 
+            // add some padding to the current message block
+            while (nobytes < 64)
+            {
+                nobytes = nobytes + 1;
+                M.e[nobytes] = 0x00; // fill block with "0"
+            }                        // end while
+        }
+        // If I have finished reading everything from the file and it was exactly 512 bits in length
+        // i.e I kept reading 64 bytes from the file & the last time I read from the file
+        // I read 64 bytes and it brought me to the exact end of the file
+        else if (feof(file))
+        {
+            S = PAD1;
+        }
+    } // read while
+
+    // If status is PAD0 or PAD1 add block padding where first 448 bits are 0's
+    // in the last block, put in the 64 bit big endian integer
+    // which represents the number of bits in the original message
+    if (S == PAD0 || S == PAD1)
+    {
+        for (i = 0; i < 56; i++)
+            M.e[i] = 0x00;
+        M.s[7] = nobits;
+    }
+    if (S == PAD1)
+    {
+        M.e[0] = 0x80;// add a "1"
+    }
     // Close the file.
     fclose(file);
 
-    for(int i = 0; i < 64; i++)
+    for (int i = 0; i < 64; i++)
     {
         // Print elements of M as 64 individual bytes.
         printf("%x ", M.e[i]);
-        printf("\n");
     }
-    
+    printf("\n");
 
     return 0;
 }
